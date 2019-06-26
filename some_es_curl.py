c136086e8a9914992043d38ea1af7f6ff211c56c
chinesepython <chinesepython@163.com>
2019-06-26 11:03:13 +0800

    xyuh

---
+++ some_es_curl.py
@@ -0,0 +1,3 @@
+# 删除index为twitter_xnr_relations doc_type为user id为834571011949469699_923752533268553728 的一条记录
+curl -XDELETE 'localhost:9206/twitter_xnr_relations/user/834571011949469699_923752533268553728'
+
